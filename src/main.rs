1ae000b45e0d1dd0701682cf5310b1d54d765a76
avnogy <avner.mindelis@gmail.com>
2023-04-02 23:46:58 +0000

    added documentation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use clap::Parser;
 use rand::Rng;
 use std::iter;
+
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author="avnogy", version, about = "A simple password generator written in Rust")]
 struct Args {
-    /// length of password 
+    /// Length of password
     #[arg(short, long, default_value_t = 8)]
     length: usize,
 
-    #[arg(short, long , action)]
+    /// Include uppercase letters
+    #[arg(short, long)]
     uppercase: bool,
 
-    #[arg(short, long , action)]
+    /// Include digits
+    #[arg(short, long)]
     digits: bool,
 
-    #[arg(short, long , action)]
+    /// Include special characters
+    #[arg(short, long)]
     special: bool,
 }
 
@@ -29,12 +33,14 @@ fn generate(length: usize,uppercase: bool,digits: bool,special: bool) -> String 
     let digits_letters = "0123456789".as_bytes();
     let special_chars = "!@#$%^&*()_+{}[];':\"\\|,.<>?/".as_bytes();
 
+    // creating the char set
     let mut char_set = Vec::new();
     char_set.extend_from_slice(lowercase_letters);
     if uppercase {char_set.extend_from_slice(uppercase_letters)};
     if digits {char_set.extend_from_slice(digits_letters)};
     if special {char_set.extend_from_slice(special_chars)};
 
+    // generating string
     let mut rng = rand::thread_rng();
     let one_char = || char_set[rng.gen_range(0..char_set.len())] as char;   
     iter::repeat_with(one_char).take(length).collect()
