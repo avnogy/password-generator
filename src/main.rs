f6a4da055a06fb756cd8137fab44da6a8a91e6f3
avnogy <avner.mindelis@gmail.com>
2023-04-03 02:40:50 +0300

    added all other flags

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,30 @@ struct Args {
 
     #[arg(short, long , action)]
     uppercase: bool,
+
+    #[arg(short, long , action)]
+    digits: bool,
+
+    #[arg(short, long , action)]
+    special: bool,
 }
 
 fn main() {
-    let length = Args::parse().length; 
-    let uppercase = Args::parse().uppercase; 
-
-
-    println!("{}",generate(length.clone(),uppercase));
+    let args = Args::parse(); 
+    println!("{}",generate(args.length,args.uppercase,args.digits,args.special));
 }
 
-fn generate(length: usize,uppercase: bool) -> String {
+fn generate(length: usize,uppercase: bool,digits: bool,special: bool) -> String {
     let lowercase_letters = "abcdefghijklmnopqrstuvwxyz".as_bytes();
     let uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes();
-    let digits = "0123456789".as_bytes();
+    let digits_letters = "0123456789".as_bytes();
     let special_chars = "!@#$%^&*()_+{}[];':\"\\|,.<>?/".as_bytes();
 
     let mut char_set = Vec::new();
     char_set.extend_from_slice(lowercase_letters);
     if uppercase {char_set.extend_from_slice(uppercase_letters)};
-    char_set.extend_from_slice(digits);
-    char_set.extend_from_slice(special_chars);
+    if digits {char_set.extend_from_slice(digits_letters)};
+    if special {char_set.extend_from_slice(special_chars)};
 
     let mut rng = rand::thread_rng();
     let one_char = || char_set[rng.gen_range(0..char_set.len())] as char;   
