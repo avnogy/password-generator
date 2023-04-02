68f5391cff62a245324a3a3facce9226d074f6f0
avnogy <avner.mindelis@gmail.com>
2023-04-03 00:55:49 +0300

    using clap for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// length of password 
+    #[arg(short, long, default_value_t = 8)]
+    length: u16,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse(); 
     println!("{:?}",&args);
 }
